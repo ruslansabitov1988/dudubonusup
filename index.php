724823b8cddbea5b6ccc7296c47e40cc6c48d54d
ruslansabitov1988 <qwertykz2010@mail.ru>
2022-01-30 22:41:29 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,90 @@
+<?php
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Raleway:wght@500&display=swap');
+    </style>
+</head>
+<body>
+    <div class="parent">
+        <div class="basic1">
+            <div class="header">
+                MENU
+                <div class="header-icon"></div>
+            </div>
+        </div>
+        <div class="basic2">
+            <div class="basic2-box">
+                <a class="basic2-text1">Best videogames totally free!</a>
+                <a class="basic2-text2">Please check our Privacy Policy and Terms & Conditions
+                </a>
+                <div class="basic2-box2">
+                    <button class="basic2-button1">SIGN IN</button>
+                    <button class="basic2-button2">Login</button>
+                </div>
+            </div>
+        </div>
+        <div class="basic3">
+            <a>Disclaimer:
+                <br>
+                Please read the terms and conditions, cookie policy and privacy policy before using our 
+                <br>
+                website. Also, please confirm that all games on this website are free. Our website won't ask 
+                <br>
+                you to use any real money anywhere. All of our videogames are totally free and are only 
+                <br>
+                placed here so that you can enjoy playing them. You may read "Our Mission" page to 
+                <br>
+                understand, why we need this website.</a>
+        </div>
+        <div class="basic4">
+            <div class="basic4-box1">
+                <div class="basic4-box1-pic1">
+                    <div class="basic4-box1-img1"></div>
+                    <a class="basic4-text1">Banditos</a>
+                </div>
+                
+                <div class="basic4-box1-pic2">
+                    <div class="basic4-box1-img2"></div>
+                    <a class="basic4-text2">Chinese Lantern</a>
+                </div>
+                <div class="basic4-box1-pic3">
+                    <div class="basic4-box1-img3"></div>
+                    <a class="basic4-text3">Diamond Cave</a>
+                </div>
+
+            </div>
+            <div class="basic4-box2">
+                <div class="basic4-box2-pic1">
+                    <div class="basic4-box2-img1"></div>
+                    <a class="basic4-text1">Golden Knight</a>
+                </div>
+            </div>
+        </div>
+        <div class="basic5">
+            <div class="basic5-box1">
+                <div class="basic5-box1-text1"> 
+                    Privacy Policy | Terms & Conditions
+                </div>
+                <div class="basic5-box1-text2"> 
+                    © | 2021
+                </div>
+            </div>
+            <div class="basic5-box2">
+                <div class="basic5-box2-text1"> 
+                    CooKies
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
